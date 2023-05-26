abf779850ce41f93d6d094011d8fe3fb764e7309
Victor Spichenkoff Santana <victorspich13@gmail.com>
2023-05-26 14:34:11 -0300

    Finished

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const db = require('./config/db')//acesso pelo knex
 app.db = db
 app.mongoose = mongoose
 
+
+const port = process.env.PORT || 2006
+
 const consign = require('consign')
 consign()
     .include('./config/passport.js')
